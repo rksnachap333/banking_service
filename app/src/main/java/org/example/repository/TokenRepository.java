d4ac5db5e1e5b91e50bd91c4557a15fc25ef9f59
RKS <rajesh@revesoft.com>
2025-07-12 20:20:13 +0530

    Token updation added

--- app/src/main/java/org/example/repository/TokenRepository.java
+++ app/src/main/java/org/example/repository/TokenRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface TokenRepository extends CrudRepository<Token, Integer> {
     Optional<Token> findByToken(String token);
+    Optional<Token> findByUserUserId(Long userId);
 }
--- app/src/main/java/org/example/services/RefreshTokenService.java
+++ app/src/main/java/org/example/services/RefreshTokenService.java
@@ -22,14 +22,28 @@ public class RefreshTokenService {
 
     public Token createRefreshToken(String username) {
         Optional<User> userExtracted = userRepository.findByUsername(username);
-        Token refreshToken = Token.builder()
-                .user(userExtracted.get())
-                .token(UUID.randomUUID().toString())
-                .expiryDate(Instant.now().plusMillis(1000*60*60*24*15)) // 15 days
-                .build();
-        return tokenRepository.save(refreshToken);
-    }
+        if (userExtracted.isPresent()) {
+            Optional<Token> token = tokenRepository.findByUserUserId(userExtracted.get().getId());
+            if(token.isPresent()){
+                //updating the new token to existing row
+                Token tokenToReplace = token.get();
+                tokenToReplace.setToken(UUID.randomUUID().toString());
+                tokenToReplace.setExpiryDate(Instant.now().plusMillis(1000 * 60 * 60 * 24 * 15));
+                return tokenRepository.save(tokenToReplace); // update existing token
+            } else{
+                //adding new token in the db
+                Token refreshToken = Token.builder()
+                        .user(userExtracted.get())
+                        .token(UUID.randomUUID().toString())
+                        .expiryDate(Instant.now().plusMillis(1000 * 60 * 60 * 24 * 15)) // 15 days
+                        .build();
+                return tokenRepository.save(refreshToken);
+            }
 
+        } else {
+            throw new UnsupportedOperationException("User not found !!");
+        }
+    }
     public Token verifyExpiration(Token token) {
         if(token.getExpiryDate().compareTo(Instant.now()) < 0) {
             tokenRepository.delete(token);
